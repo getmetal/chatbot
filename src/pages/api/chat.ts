33af35d865bb2c06d69ebad678c6685627f6c1a4
James O'Dwyer <james@getmetal.io>
2023-07-10 11:42:08 -0400

    rm augmentation

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -1,6 +1,7 @@
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 import { Configuration, OpenAIApi } from 'openai-edge';
-import { retrieveContext } from '../../helpers/metal';
+import { retrieveContext } from '@/helpers/metal';
+import { DEFAULT_PROMPT } from '@/helpers/prompts';
 import { encode } from 'gpt-tokenizer';
 
 export const config = {
@@ -13,53 +14,8 @@ const openAiConfig = new Configuration({
 
 const openai = new OpenAIApi(openAiConfig)
 
-const DEFAULT_SYSTEM_MSG_CONTENT = `You are an AI Assistant that is an intelligent chatbot designed to answer questions related to the context provided.
-  The context is contained in the text delimited by triple quotes.
-  Questions can be related to the context or previous chat messages within the conversation.
-  If the question is a follow up to a previous question, make sure to reference prior questions and answers.
-  When appropriate, include a formatted citation to the source of the answer.
-  If you're not sure of an answer, say 'I don't know'.`;
-
 const DEFAULT_TEMPERATURE = 0;
 
-async function getRetrievalQuery(messages: any[], last: any) {
-  const origQ = last?.content;
-
-  last.content = `Question: ${origQ}
-    Answer:`;
-
-  const messagesWSystem = [
-    {
-      role: 'system',
-      content: `Generate a text query that captures the objective of the question below.
-      You must generates a fully-formed, semantically rich question factoring in previous chat messages.
-      This text query will be used to run a retrieval of context that will be used to answer the below question.
-      It should be as semantically rich as possible.
-      Do not go beyond 25 tokens.
-      It is critical that this generated query fetches as much relevant context as possible.
-      If you do not know, return with the "question" asked.
-      `,
-    },
-    ...messages,
-    last,
-  ]
-
-  const res = await openai.createChatCompletion({
-    model: 'gpt-4',
-    messages: messagesWSystem,
-    max_tokens: 35,
-  });
-
-  if (res?.status === 400) {
-    throw new Error('Context window exceeded. Please clear history and try again.')
-  }
-
-  const json = await res.json();
-  const txt = json?.choices.reduce((acc: any, curr: any) => {
-    return acc + curr.message.content;
-  }, '')
-  return txt || origQ;
-}
 
 function getTokenCount(messages: any[]) {
   return new Promise((resolve, _reject) => {
@@ -79,12 +35,11 @@ export default async function handler(req: any) {
 
   try {
     const last = messages.pop();
-    const retrievalQResponse = await getRetrievalQuery(messages, { ...last });
-
-    const responseQ = { ...last };
-    const ctx = await retrieveContext(retrievalQResponse, {
+    const ctx = await retrieveContext(last.content, {
       limit: chunkCount
     });
+
+    const responseQ = { ...last };
 
     responseQ.content = `
       Context: '''${ctx}'''
@@ -95,7 +50,7 @@ export default async function handler(req: any) {
     const messagesWSystem = [
       {
         role: "system",
-        content: system || DEFAULT_SYSTEM_MSG_CONTENT,
+        content: system || DEFAULT_PROMPT,
       },
       ...messages,
       responseQ,
